d8ea22bb6c0739ed7b48fb40afcf9300926ae02e
DriftingStarry <doubichenzi@126.com>
2024-02-03 18:38:10 +0800

    reptile

--- reptile/main.py
+++
@@ -1,6 +0,0 @@
-import requests as rq
-from bs4 import BeautifulSoup
-
-rep = rq.get("https://news.hqu.edu.cn/hdyw.htm")
-if rep.ok:
-    print(1)
---
+++ reptile/reptile.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import requests as rq
+from bs4 import BeautifulSoup
+
+rep = rq.get("https://news.hqu.edu.cn/hdyw.htm")
+rep.encoding = 'utf-8'
+
+urls = []
+
+if rep.ok:
+    html = rep.text
+    soup = BeautifulSoup(html,"html.parser")
+    for i in range(10):
+        lis = soup.findAll("li" , attrs={"id" : "line_u11_%d"%(i)})
+        for l in lis:
+            title = l.h3.text
+            href = l.a['href']
+            urls.append([title,"https://news.hqu.edu.cn/"+href])
+for url in urls:
+    print(url)
