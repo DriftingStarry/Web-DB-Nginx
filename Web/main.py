b231fcc0406ec7c5087800c79854f232eebe103d
DriftingStarry <doubichenzi@126.com>
2024-02-04 13:30:34 +0800

    update

--- Web/main.py
+++ Web/main.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world!"
+    return render_template("index.html")
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
 if __name__ == "__main__":
     app.run(port=11451,host="127.0.0.1")
