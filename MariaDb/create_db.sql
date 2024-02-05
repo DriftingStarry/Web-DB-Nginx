17606e64cd1101c49403d4248efa74f3113802b4
DriftingStarry <doubichenzi@126.com>
2024-02-05 14:34:32 +0800

    update

--- MariaDb/create_db.sql
+++ MariaDb/create_db.sql
@@ -2,6 +2,5 @@ CREATE DATABASE hddata;
 USE hddata;
 CREATE TABLE news(
 title TEXT,
-turl TEXT,
-id INT PRIMARY KEY
+turl TEXT
 );
--- reptile/reptile.py
+++ reptile/reptile.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
 import requests as rq
 from bs4 import BeautifulSoup
+import pymysql
 
-rep = rq.get("https://news.hqu.edu.cn/hdyw.htm")
-rep.encoding = 'utf-8'
+def reptile(web):
+    rep = rq.get(web)
+    rep.encoding = 'utf-8'
+    urls = []
+    if rep.ok:
+        html = rep.text
+        soup = BeautifulSoup(html,"html.parser")
+        for i in range(10):
+            lis = soup.findAll("li" , attrs={"id" : "line_u11_%d"%(i)})
+            for l in lis:
+                title = l.h3.text
+                href = l.a['href']
+                urls.append([title,"https://news.hqu.edu.cn/"+href])
+    for url in urls:
+        print(url)
+    return urls
 
-urls = []
+def dbwrite(host,port,url):
+    conn = pymysql.connect(host=host,port=port,user='PyReptile',password='Py_114514',database='hddata')
+    cur = conn.cursor()
+    sql = "insert into news (title,turl) values (%s,%s)"%(url[0],url[1])
+    cur.execute(sql)
+    conn.commit()
 
-if rep.ok:
-    html = rep.text
-    soup = BeautifulSoup(html,"html.parser")
-    for i in range(10):
-        lis = soup.findAll("li" , attrs={"id" : "line_u11_%d"%(i)})
-        for l in lis:
-            title = l.h3.text
-            href = l.a['href']
-            urls.append([title,"https://news.hqu.edu.cn/"+href])
+web = "https://news.hqu.edu.cn/hdyw.htm"
+host = 'localhost'
+port = 3306
+urls = reptile(web)
 for url in urls:
-    print(url)
-
-lis = open("./list.txt" , "w+" , encoding = "utf-8")
-for url in urls:
-    for s in url:
-        lis.writelines(s)
-lis.close()
+    dbwrite(host,port,url)
